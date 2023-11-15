85e72eaba65e464c20473b69fe50404fcf551e18
Parul Bedi <parul@dits.com>
2023-11-15 12:55:41 +0530

    source code added

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hi there!! welcome!!!!
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      <p>
+          i was changed again
+        </p>
+    </div>
+  );
+}
+
+export default App;
